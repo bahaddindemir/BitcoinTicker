b8c0829078df6ade2a131bf796100fca82a34a50
Bahaddin Demir <demirbahaddin@hotmail.com>
2026-03-15 21:54:49 +0300

    Migrate KAPT libraries to KSP and update deprecated Gradle configs

--- buildSrc/src/main/java/com/bahaddindemir/bitcointicker/buildsrc/Config.kt
+++ buildSrc/src/main/java/com/bahaddindemir/bitcointicker/buildsrc/Config.kt
@@ -4,8 +4,8 @@ object Config {
     object AppConfig {
         const val appId = "com.bahaddindemir.bitcointicker"
         const val minSdkVersion = 23
-        const val compileSdkVersion = 35
-        const val targetSdkVersion = 35
+        const val compileSdkVersion = 36
+        const val targetSdkVersion = 36
         const val versionCode = 1
         const val versionName = "1"
         const val testRunner = "androidx.test.runner.AndroidJUnitRunner"

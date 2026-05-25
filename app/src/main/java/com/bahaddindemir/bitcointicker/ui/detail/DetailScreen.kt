0e8bacedc6748bd95c2ee488450c90018bfe60f8
Bahaddin Demir <demirbahaddin@hotmail.com>
2026-05-25 23:25:14 +0300

    View Binding removed and unused resources deleted.

--- app/src/main/java/com/bahaddindemir/bitcointicker/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/bahaddindemir/bitcointicker/ui/detail/DetailScreen.kt
@@ -32,8 +32,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -22,7 +22,6 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
       }
 
       buildFeatures {
-        viewBinding = true
         buildConfig = true
         compose = true
       }
